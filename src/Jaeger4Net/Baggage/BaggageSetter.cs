9dcaccac0b21a9fe514d1187b9bd0ff201080751
Korir <gideonkorir@gmail.com>
2017-09-10 19:45:40 +0300

    Added baggage setter

---
+++ src/Jaeger4Net/Baggage/BaggageSetter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jaeger4Net.Baggage
+{
+    public class BaggageSetter
+    {
+        readonly IRestrictBaggage baggageRestrictor;
+
+        public BaggageSetter(IRestrictBaggage baggageRestrictor)
+        {
+            this.baggageRestrictor = baggageRestrictor ?? throw new ArgumentNullException(nameof(baggageRestrictor));
+        }
+
+        public SpanContext SetBaggage(Span span, string key, string value)
+        {
+            var restriction = baggageRestrictor.Get(span.ServiceName, key);
+            string prevItem = null;
+            if (!restriction.KeyAllowed)
+            {
+                //todo:update metrics and log fields
+                return span.Context as SpanContext;
+            }
+            if (value.Length > restriction.MaxValueLength)
+            {
+                value = value.Substring(0, restriction.MaxValueLength);
+                //todo: update metrics
+            }
+            prevItem = span.GetBaggageItem(key);
+            //log and update metrics
+            return (span.Context as SpanContext).AppendBaggageItem(key, value);
+        }
+    }
+}
