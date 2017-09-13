91cfe697948bca0e58d883fcc6187c6516e2e9c5
Gideon Korir <gideon@m-kopa.com>
2017-09-13 16:24:54 +0300

    Added composite and in-memory reporter

---
+++ src/Jaeger4Net/Reporters/CompositeReporter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jaeger4Net.Reporters
+{
+    public class CompositeReporter : IReporter
+    {
+        readonly List<IReporter> reporters;
+
+        public CompositeReporter(params IReporter[] reporters)
+        {
+            this.reporters = new List<IReporter>(reporters);
+        }
+
+        public int Report(Span span)
+        {
+            var rem = 0;
+            foreach (var r in reporters)
+                rem = r.Report(span);
+            return rem;
+        }
+
+        public void Dispose()
+            => reporters.ForEach(c => c.Dispose());
+    }
+}
---
+++ src/Jaeger4Net/Reporters/InMemoryReporter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jaeger4Net.Reporters
+{
+    class InMemoryReporter : IReporter
+    {
+        readonly List<Span> spans;
+
+        public IReadOnlyList<Span> Spans => spans;
+
+        public InMemoryReporter()
+        {
+            spans = new List<Span>();
+        }
+
+        public int Report(Span span)
+        {
+            lock (spans)
+            {
+                spans.Add(span);
+                return spans.Count;
+            }
+
+        }
+        public void Dispose()
+        {
+            lock (spans)
+                spans.Clear();
+        }
+    }
+}
