5e0edb97a9a64906708523b93e4a1fb43c0a72ba
Gideon Korir <gideon@m-kopa.com>
2017-09-19 10:47:42 +0300

    Baggage Tests completed

---
+++ src/Jaeger4Net.Tests/Baggage/MaxLengthBaggageRestrictorTests.cs
@@ -0,0 +1,20 @@
+﻿using Jaeger4Net.Baggage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Jaeger4Net.Tests.Baggage
+{
+    public class MaxLengthBaggageRestrictorTests
+    {
+        [Fact]
+        public void TestGetRestriction()
+        {
+            var restrictor = new MaxLengthBaggageRestrictor();
+            var key = "key";
+            var actual = restrictor.Get(string.Empty, key);
+            Assert.Equal(new Restriction(true, 2048), actual);
+        }
+    }
+}
